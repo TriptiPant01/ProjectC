04b4f6b53df7894a40276780b0aa8f39680bf363
Sabina777 <crupee@crupees-iMac.local>
2020-03-03 14:07:44 +0545

    changes

--- src/colors/index.js
+++ src/colors/index.js
@@ -5,10 +5,4 @@ export default Colors = {
   backgroundColor: '#F1F2F3',
   cardHolderBg: '#F4F4F4',
   whiteColor: '#FFFFFF',
-   InactiveButton: '#EBEBEB',
-    Inactivebuttontext: '#0000004D',
-    activeButton: '#43BBF0',
-    activebuttontext: '#FFFFFF',
-    appColor:'#FFFFFF',
-    textBoxBorder:'#43BBF0'
 };
